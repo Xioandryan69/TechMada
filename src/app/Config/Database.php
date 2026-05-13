f2643fc496a05124b1f291f2561398f039f25cbf
Andrianandrainy Ny Avo <andriannyavo9@gmail.com>
2026-05-13 10:44:00 +0300

    ok

--- src/app/Config/Database.php
+++ src/app/Config/Database.php
@@ -59,7 +59,7 @@ class Database extends Config
     //     * @var array<string, mixed>
     //     */
         public array $default = [
-            'database'    => 'database.db',
+            'database'    => WRITEPATH . 'database.db',
             'DBDriver'    => 'SQLite3',
             'DBPrefix'    => '',
             'DBDebug'     => true,
