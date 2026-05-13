a35d5133e300446d7c74f820f021e67e5b2f3206
Gaelle <raharimirantyannelle@gmail.com>
2026-05-13 10:12:01 +0300

    repartiotion des taches

--- base/addcolumn.sql
+++
@@ -1,4 +0,0 @@
-ALTER TABLE regimes ADD COLUMN variation_poids DECIMAL(5, 2) DEFAULT 0;
-ALTER TABLE regimes ADD COLUMN pourcentage_viande INT DEFAULT 0;
-ALTER TABLE regimes ADD COLUMN pourcentage_poisson INT DEFAULT 0;
-ALTER TABLE regimes ADD COLUMN pourcentage_volaille INT DEFAULT 0;
